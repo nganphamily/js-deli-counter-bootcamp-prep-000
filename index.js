a2b221ae733e9fd640d44d4b6c4c3f5d5a2ed7b7
NGAN PHAM <nganvpham@gmail.com>
2018-01-29 01:39:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine(line){
   if (line.length > 0 ){
     var newLine = [];
     for (let i =0; i < line.length; i++){
-      newLine.push(`${i + 1}. ${line[i]} `);
+      newLine.push(` ${i + 1}. ${line[i]}`);
     }
     return`The line is currently ${newLine}.`;
   }else{
