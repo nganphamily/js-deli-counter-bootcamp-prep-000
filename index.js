3517f514222e5b4ca7ed8b8301b928354b5d8b36
NGAN PHAM <nganvpham@gmail.com>
2018-01-29 00:49:36 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length>0) {
+    return `Currently serving ${katzDeliLine.shift()}.` ;
+  }
+  else {
+    return "There is nobody waiting to be served!";
+  }
+}
+
+function currentLine(line){
+  if ( ){
+    return 'The line is currently ${line[i]}';
+
+  }else{
+    return "The line is currently empty.";
+  }
+}
