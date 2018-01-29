8af8255b141b659ccbc15d1646862cb228d7d24f
NGAN PHAM <nganvpham@gmail.com>
2018-01-29 01:38:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ function nowServing(katzDeliLine){
 
 function currentLine(line){
   if (line.length > 0 ){
-    return`The line is currently ${line}.`;
+    var newLine = [];
+    for (let i =0; i < line.length; i++){
+      newLine.push(`${i}. ${line[i]}`);
+    }
+    return`The line is currently ${newLine}.`;
   }else{
     return "The line is currently empty.";
   }
